527ae2aa5103daa083af855e962f9957e4fc420a
Victoria <mariagrisi@cc.ci.ufpb.br>
2022-09-11 22:48:14 -0300

    starting circular lists

--- Lists/Circular Linked List/circular_linked_list.c
+++ Lists/Circular Linked List/circular_linked_list.c
@@ -18,3 +18,193 @@ typedef struct tCircular_List{
 
 
 //////////////////////////////////////////////////////////////////
+
+
+tNode * create_node(tNode * node, int data_value){
+    node = (tNode *)malloc(sizeof(tNode));
+
+    if(node == NULL){
+        printf("couldn't allocate node :(\n");
+        return NULL;
+    }
+
+    node->data = data_value;
+    node->next = NULL;
+    node->previous = NULL;
+
+    printf("memory allocated node, value = %d\n", node->data);
+
+    return node;
+}
+
+tCircular_List * create_list(tCircular_List * list){
+    list->head = NULL;
+    list->total_nodes = 0;
+
+    printf("list created!\n");
+
+    return list;
+}
+
+int is_empty(tCircular_List * list){
+    if(list->head == NULL){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int get_list_size(tCircular_List * list){
+    return list->total_nodes;
+}
+
+int get_value_at_position(tCircular_List * list, int index){
+    int i = 1;
+    tNode * node_get_position = list->head;
+
+    if(index == 1){
+        return node_get_position->data;
+    }
+    else{
+        for(i; i < index - 1; i++){
+            node_get_position = node_get_position->next;
+        }
+    }
+    return node_get_position->data;
+}
+
+int modify_value_at_position(tCircular_List * list, int new_value, int index){
+    int old_value; 
+    int i = 1;
+    tNode * node_get_position = list->head;
+
+    for(i; i < index; i++){ 
+        if(node_get_position->next == NULL){
+            break;
+        }
+        node_get_position = node_get_position->next;
+    }
+
+    old_value = node_get_position->data;
+    node_get_position->data = new_value;
+
+    if(node_get_position->data == old_value){
+        return 0;
+    }
+    else{
+        return 1;  
+    } 
+}
+
+int insert_at_position(tCircular_List * list, int data_value, int index){
+    tNode * new_node;
+    tNode * node_get_position = list->head;
+    int i = 1;
+
+    new_node = create_node(new_node, data_value);
+
+    if(new_node == NULL){
+        printf("couldn't allocate node and insert it :(\n");
+        return 0;
+    }
+
+    else{
+        if(index == 0 || index > get_list_size(list) + 1){
+            printf("not a valid index\n");
+            return 0;
+        }
+
+        if(index == 1){
+            list->head = new_node;
+        }
+
+        else{
+            for(i; i < index; i++){
+                if(node_get_position->next == NULL){
+                    break;
+                }
+                node_get_position = node_get_position->next;
+            }
+            printf("\ni = %d, node data = %d\n\n", i, node_get_position->data);
+
+            new_node->previous = node_get_position;
+            new_node->next = node_get_position->next;
+            node_get_position->next = new_node;
+        }
+
+        list->total_nodes++;
+
+        return 1;
+    }
+}
+
+int remove_at_position(tCircular_List * list, int index){
+    int i = 1;
+    tNode * control_node = list->head;
+    tNode * remove_node;
+
+    if(index == 0 || index > get_list_size(list)){
+        printf("not a valid index\n");
+        return 0;
+    }
+
+    else if(index == 1){
+        remove_node = control_node;
+        control_node = remove_node->next;
+    }
+
+    else{
+        for(i; i < index - 1; i++){ 
+                if(control_node->next == NULL){
+                    break;
+                }
+                control_node = control_node->next;
+        }
+        remove_node = control_node->next;
+        if(remove_node->next == NULL){
+            control_node->next == NULL;
+        }
+        else{
+            control_node->next = remove_node->next; 
+        }
+        control_node->next->previous = control_node;
+    }
+    free(remove_node);
+
+    list->total_nodes--;
+
+    return 1;
+}
+
+void print_list(tCircular_List * list){
+    int i = 1;
+    tNode * node_print = list->head;
+
+    for(i; i <= get_list_size(list); i++){
+        if(node_print->next == NULL){
+            printf("index = %d, data = %d\n", i, node_print->data);
+            break;
+        }
+        else{
+            printf("index = %d, data = %d\n", i, node_print->data);
+            node_print = node_print->next;
+        }
+    }
+    printf("\n");
+}
+
+void free_list(tCircular_List * list){
+    int i = 1, list_size = get_list_size(list);
+
+    printf("starting to clean...\n");
+    for(i; i <= list_size;){
+        printf("actual size = %d, i = %d\n", list_size, i);
+        list_size = get_list_size(list);
+        printf("actual size = %d, i = %d\n", list_size, i);
+        remove_at_position(list, list_size);
+    }
+    
+    printf("list cleaned!\n");
+    list->head = NULL;
+}
